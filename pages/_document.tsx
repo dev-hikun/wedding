bd7cb1e3e33edf52ec83417f570f6eb18557b252
Harry <dev.hikun@gmail.com>
2023-07-17 03:44:22 +0900

    package: google tag manager 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 
 import { Html, Main, NextScript, Head } from 'next/document';
+import Script from 'next/script';
 
 import { ja, ko } from 'assets/fonts';
 import type { Locale } from 'types/common';
@@ -8,7 +9,13 @@ import type { Locale } from 'types/common';
 const document: FC<Locale> = ({ locale }) => {
   return (
     <Html lang={locale}>
-      <Head />
+      <Head>
+        <Script id="gtag" strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-HV9ZMJHDB5">
+          {
+            "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-HV9ZMJHDB5');"
+          }
+        </Script>
+      </Head>
       <body className={[ja.variable, ko.variable].join(' ')}>
         <Main />
         <NextScript />
